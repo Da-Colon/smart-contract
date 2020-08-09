e339659f6ede06b5bebedafaf6e4b2caf334412a
Da-Colon <dcolonantonio@gmail.com>
2020-08-09 10:34:59 -0400

    initial commit

---
+++ smart-contract/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-6 bg-blue-700">
+      <h1 className="text-white text-xl">Smart Contract</h1>
+    </div>
+  );
+}
+
+export default App;
